7f09e51ccc206e5de51235e13918ddf1c05f575b
celesteMoon <notCelesteMoon@gmail.com>
2024-11-01 19:53:53 +0800

    remake

--- rps/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, re_path
-from . import consumers
-
-rps_urlpatterns = [
-    # path('ws/game/', consumers.RPSConsumer.as_asgi()),
-    re_path(r'ws/game/(?P<game_id>\w+)/$', consumers.RPSConsumer.as_asgi()),
-]
--- rps/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import game_view, lobby_view
-
-urlpatterns = [
-    path('', lobby_view, name='lobby'),
-    path('game/<str:game_id>/', game_view, name='game'),
-]
