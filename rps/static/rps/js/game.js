9fd3335e955599b123972d0d0bed3c2f6eae0c51
celesteMoon <notCelesteMoon@gmail.com>
2024-11-02 16:30:18 +0800

    add links to profile in rps

--- rps/static/rps/js/game.js
+++ rps/static/rps/js/game.js
@@ -1,68 +1,74 @@
-const statusElement = document.getElementById('status');
-const choicesElement = document.getElementById('choices_div');
-const timerElement = document.getElementById('timer');
-const scoreElement = document.getElementById('score');
-const selfChoiceElement = document.getElementById('emoji_left');
-const opponentChoiceElement = document.getElementById('emoji_right');
-const selfNameElement = document.getElementById('name_left');
-const opponentNameElement = document.getElementById('name_right');
+const statusElement = document.getElementById("status");
+const choicesElement = document.getElementById("choices_div");
+const timerElement = document.getElementById("timer");
+const scoreElement = document.getElementById("score");
+const selfChoiceElement = document.getElementById("emoji_left");
+const opponentChoiceElement = document.getElementById("emoji_right");
+const selfNameElement = document.getElementById("name_left");
+const opponentNameElement = document.getElementById("name_right");
 var isPlayer = false;
-let gameId = JSON.parse(document.getElementById('game-id').textContent);
-console.log('gameId: ' + gameId.toString())
+let gameId = JSON.parse(document.getElementById("game-id").textContent);
+console.log("gameId: " + gameId.toString())
 
-const socket = new WebSocket('ws://' + window.location.host + '/ws/game/' + gameId + '/');
+selfNameElement.href = '/profile/' + selfNameElement.textContent;
 
+const socket = new WebSocket("ws://" + window.location.host + "/ws/game/" + gameId + "/");
 socket.onmessage = function(event)
 {
     const data = JSON.parse(event.data);
     console.log(data);
-    if (data.status === 'waiting')
+    if (data.status === "waiting")
     {
-        statusElement.textContent = '状态：' + data.status;
+        statusElement.textContent = "状态：" + data.status;
     }
-    else if (data.status === 'game_start')
+    else if (data.status === "game_start")
     {
         opponentNameElement.textContent = data.opponent_name;
+        selfNameElement.href = '/profile/' + selfNameElement.textContent;
+        opponentNameElement.href = '/profile/' + opponentNameElement.textContent;
         gameId = data.game_id.toString();
         console.log("gameid: "+gameId);
         isPlayer = true;
     }
-    else if (data.status === 'round_start')
+    else if (data.status === "round_start")
     {
         countdown(3);
         delay(function(){
-            choicesElement.style.display = 'flex'; // 显示选项
-            scoreElement.textContent = data.self_score + ':' + data.opponent_score;
+            choicesElement.style.display = "flex"; // 显示选项
+            scoreElement.textContent = data.self_score + ":" + data.opponent_score;
             opponentChoiceElement.textContent = "❔";
             selfChoiceElement.textContent = "❔";
         }, 3)
     }
-    else if (data.status === 'round_end')
+    else if (data.status === "round_end")
     {
         if (data.opponent_choice == "rock") opponentChoiceElement.textContent = "🪨";
         else if (data.opponent_choice == "scissors") opponentChoiceElement.textContent = "✂️";
         else if (data.opponent_choice == "paper") opponentChoiceElement.textContent = "📃";
-        scoreElement.textContent = data.self_score + ':' + data.opponent_score;
-        statusElement.textContent = '你的选择：' + data.your_choice + '，对手选择：' + data.opponent_choice + '，结果：' + data.result;
-        choicesElement.style.display = 'none'; // 隐藏选项
+        scoreElement.textContent = data.self_score + ":" + data.opponent_score;
+        statusElement.textContent = "你的选择：" + data.your_choice + "，对手选择：" + data.opponent_choice + "，结果：" + data.result;
+        choicesElement.style.display = "none"; // 隐藏选项
 
         if (Math.max(data.self_score, data.opponent_score) == 3)
         {
-            if (data.self_score == 3) selfNameElement.textContent += '👑';
-            if (data.opponent_score == 3) opponentNameElement.textContent += '👑';
+            if (data.self_score == 3) selfNameElement.textContent += "👑";
+            if (data.opponent_score == 3) opponentNameElement.textContent += "👑";
         }
     }
-    else if (data.status === 'spectate_start')
+    else if (data.status === "spectate_start")
     {
         if (isPlayer) return ;
-        statusElement.textContent = '这里满员了, 你现在是旁观者模式!';
+        statusElement.textContent = "这里满员了, 你现在是旁观者模式!";
         selfNameElement.textContent = data.p1_name;
+        selfNameElement.href = '/profile/' + selfNameElement.textContent;
         opponentNameElement.textContent = data.p2_name;
+        opponentNameElement.href = '/profile/' + opponentNameElement.textContent;
         selfChoiceElement.textContent = "❔";
         opponentChoiceElement.textContent = "❔";
-        scoreElement.textContent = data.p1_score + ':' + data.p2_score;
+        scoreElement.textContent = data.p1_score + ":" + data.p2_score;
+
     }
-    else if (data.status === 'spectate_update')
+    else if (data.status === "spectate_update")
     {
         if (isPlayer) return ;
         selfNameElement.textContent = data.p1_name;
@@ -73,12 +79,12 @@ socket.onmessage = function(event)
         if (data.p2_choice == "rock") opponentChoiceElement.textContent = "🪨";
         else if (data.p2_choice == "scissors") opponentChoiceElement.textContent = "✂️";
         else if (data.p2_choice == "paper") opponentChoiceElement.textContent = "📃";
-        scoreElement.textContent = data.p1_score + ':' + data.p2_score;
+        scoreElement.textContent = data.p1_score + ":" + data.p2_score;
 
         if (Math.max(data.p1_score, data.p2_score) == 3)
         {
-            if (data.p1_score == 3) selfNameElement.textContent += '👑';
-            if (data.p2_score == 3) opponentNameElement.textContent += '👑';
+            if (data.p1_score == 3) selfNameElement.textContent += "👑";
+            if (data.p2_score == 3) opponentNameElement.textContent += "👑";
         }
         else
         {
@@ -97,10 +103,15 @@ function makeChoice(choice)
     else if (choice == "scissors") selfChoiceElement.textContent = "✂️";
     else if (choice == "paper") selfChoiceElement.textContent = "📃";
     socket.send(JSON.stringify({
-        'status': 'choice',
-        'game_id': gameId,
-        'choice': choice
+        "status": "choice",
+        "game_id": gameId,
+        "choice": choice
     }));
+}
+
+function toProfile(name)
+{
+    window.open("/profile/"+name);
 }
 
 function countdown(cd)
