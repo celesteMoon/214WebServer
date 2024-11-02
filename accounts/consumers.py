720f0d10a80a48eb6473a4a3d05ddb5a2df002d7
celesteMoon <notCelesteMoon@gmail.com>
2024-11-02 13:48:44 +0800

    add profile

--- accounts/consumers.py
+++ accounts/consumers.py
@@ -9,12 +9,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "214web.settings")
 django.setup()
 
 from .models import CustomUser
+from django.utils import timezone
 from django.template.defaultfilters import date
-from asgiref.sync import sync_to_async
-from django.core.exceptions import ObjectDoesNotExist
-from channels.db import database_sync_to_async
-from django.shortcuts import get_object_or_404
-
 
 logger = logging.getLogger('accounts/consumers.py')
 
@@ -22,55 +18,25 @@ class ProfileConsumer(WebsocketConsumer):
     def connect(self):
         self.username = self.scope["url_route"]["kwargs"]["username"]
         self.accept()
-        # try:
-        #     user = CustomUser.objects.get(username=self.username)
-        #     user_exists = True
-        # except ObjectDoesNotExist:
-        #     user_exists = False
-        #     await self.send(text_data=json.dumps({
-        #         'error': 'error'
-        #     }))
-        #     return
-        
-        # self.user = self.get_user(self.username)
-        # if self.user == False:
-        #     logger.info("user does not exist")
-        #     await self.send(text_data=json.dumps({
-        #         'error': 'error'
-        #     }))
-        #     return
-        # logger.info("sb!!!")
-        # await self.send(text_data=json.dumps({
-        #     'username': self.get_user(self.username),
-        #     'join_date': date(self.user.date_joined, "Y M jS H:i:s O"),
-        #     'rps_win': self.user.stats_rps_win,
-        #     'rps_lose': self.user.stats_rps_lose,
-        # }))
         text_data = self.get_user(self.username)
         if text_data is not None:
             self.send(text_data=text_data)
         else:
-            self.send(text_data=json.dumps({
-                'error': 'error'
-            }))
+            self.send(text_data=json.dumps({'error': 'error'}))
 
     def disconnect(self, code):
         return super().disconnect(code)
     
     def receive(self, text_data=None, bytes_data=None):
         return super().receive(text_data, bytes_data)
-    
-    # @database_sync_to_async
+
     def get_user(self, username):
         if not CustomUser.objects.filter(username=username).exists():
             return None
         user = CustomUser.objects.get(username=username)
         return json.dumps({
             'username': user.username,
-            'join_date': date(user.date_joined, "Y M jS H:i:s O"),
+            'join_date': date(timezone.localtime(user.date_joined), "Y M jS H:i:s O"),
             'rps_win': user.stats_rps_win,
             'rps_lose': user.stats_rps_lose,
         })
-        
-
-        # return get_object_or_404(CustomUser, username=username)
--- accounts/static/accounts/js/profile.js
+++ accounts/static/accounts/js/profile.js
@@ -1,8 +1,11 @@
+const infoElement = document.getElementById("info")
+const userNameElement = document.getElementById("user_name")
 const joinDateElement = document.getElementById("join_date")
 const rpsWinLoseElement = document.getElementById("rps_win_lose");
 
 let username = JSON.parse(document.getElementById("username").textContent);
-console.log("username: " + username.toString())
+userNameElement.textContent = username;
+console.log("username: " + username.toString());
 
 const socket = new WebSocket("ws://" + window.location.host + "/ws/profile/" + username + "/");
 
@@ -13,9 +16,11 @@ socket.addEventListener("message", function (event)
     console.log(data);
     if (data.error == 'error')
     {
-        alert("该用户不存在")
+        infoElement.textContent = "该用户不存在 :(";
+        infoElement.style.display = "block";
         return ;
     }
-    joinDateElement.textContent = `${data.join_date} 加入`
-    rpsWinLoseElement.textContent = `胜/负: ${data.rps_win}/${data.rps_lose}`
+    infoElement.style.display = "block";
+    joinDateElement.textContent = `${data.join_date} 加入`;
+    rpsWinLoseElement.textContent = `胜/负: ${data.rps_win}/${data.rps_lose}`;
 })
